d299fc7165655459ee425c82bf1f0b73e477de1d
Matt <84277551+MattRueter@users.noreply.github.com>
2026-02-21 15:17:25 +0100

    create main file to execute test runner and reconfigure single request to mirror array like structure.

---
+++ create_gapped_sentences/gapped_sentences.py
@@ -0,0 +1,26 @@
+import anthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+client = anthropic.Anthropic()
+
+
+def generate_gapped_sentences(prompt, req):
+  # Make a call to LLM with a single prompt and requst.
+  print("Calling Claude")
+
+  message = client.messages.create(
+    model="claude-sonnet-4-6",
+    max_tokens=1000,
+    temperature=0.5,
+    system=prompt,
+    messages=[
+        {
+            "role": "user",
+            "content": req
+        }
+    ]
+  )
+  print(message.content)
+
+
---
+++ create_gapped_sentences/main.py
@@ -0,0 +1,59 @@
+import json
+from pathlib import Path
+from gapped_sentences import generate_gapped_sentences
+
+#get json file and unpack "example_requests array and JSONify"
+req_json = Path("single_req.json").read_text(encoding="utf-8")
+req_json = json.loads(req_json)
+req_json = req_json["example_requests"]
+#req_json = json.dumps(req_json)
+
+
+#get system prompt
+prompt_path = Path(__file__).resolve().parent /"system.txt"
+prompt = prompt_path.read_text(encoding="utf-8")
+
+
+
+
+##################################################################
+## Write helper functions here and then move to root. ----------------------------------------
+
+def iterator(prompt, reqs):
+  results = []
+  
+  for req in reqs:
+    results.append(req)
+  return results
+
+
+###################################################################
+
+
+
+
+
+# generation function
+def run_eval_suite(generate=False, prompt=False, req=False, results_path=False, evaluate=False, eval_prompt=False, evaluation_results_path=False):
+  # call iterator and call generate on each iteration (use prompt, and req as args)
+  # e.g. results = iterator(generate, prompt, req)
+
+  results = iterator(prompt, req_json)
+  print(results)
+
+  # save results to file (save as json)
+  # e.g. save(results_path)
+
+  # call evaluate_iterator and call evaluate on each iteration (use evaluate, eval_prompt, and results as args )
+  # evaluation_results = evaluate_iterator(evaluate, eval_prompt, results)
+
+  # save evaluation_results to file (save as json)
+  # e.g. save(evaluation_results_path)
+
+  # get average score. Call calculate_score (use evaluation_results as args). 
+  # write score message and append to file at evaluation_results_path
+
+
+
+run_eval_suite(prompt=prompt, req=req_json)
+
