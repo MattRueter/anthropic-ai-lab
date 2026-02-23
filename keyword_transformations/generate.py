55a6dad6ae1ace0b7532afa4633fe0c7a3e70322
Matt <84277551+MattRueter@users.noreply.github.com>
2026-02-23 19:50:45 +0100

    create draft keyword_transformations feature

---
+++ keyword_transformations/generate.py
@@ -0,0 +1,25 @@
+import anthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+client = anthropic.Anthropic()
+
+
+def generate_keyword_transformation(prompt, req):
+  # Make a call to LLM with a single prompt and requst.
+  print("Calling Claude")
+
+  message = client.messages.create(
+    model="claude-sonnet-4-6",
+    max_tokens=1000,
+    temperature=0.5,
+    system=prompt,
+    messages=[
+        {
+            "role": "user",
+            "content": req
+        }
+    ]
+  )
+  return message.content[0].text
+
---
+++ keyword_transformations/main.py
@@ -0,0 +1,35 @@
+import json
+from dotenv import load_dotenv
+from pprint import pprint
+from pathlib import Path
+
+from keyword_transformations.generate import generate_keyword_transformation
+from keyword_transformations.evaluate import evaluate_keyword_transformation
+
+from eval_suite.run_eval_suite import run_eval_suite
+from eval_suite.config import create_eval_config
+
+
+config = create_eval_config(
+    base_dir=Path(__file__).resolve().parent,
+    data_file="starter_dataset.json",
+)
+
+# TODO decide how best to...
+# ...unpack variables.
+req_dict = config.req_dict
+prompt = config.prompt
+eval_prompt = config.eval_prompt
+results_path = config.results_path
+evaluation_results_path = config.evaluation_results_path
+
+run_eval_suite(
+  generate=generate_keyword_transformation,
+  prompt=prompt, 
+  reqs=req_dict, 
+  results_path=results_path,
+  evaluate=evaluate_keyword_transformation,
+  eval_prompt=eval_prompt,
+  evaluation_results_path=evaluation_results_path
+ )
+
