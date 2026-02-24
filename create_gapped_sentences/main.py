92b5c740038ab2cd5f0a69649f11e60bd7fafb33
Matt <84277551+MattRueter@users.noreply.github.com>
2026-02-24 15:35:26 +0100

    move config file to root.

--- create_gapped_sentences/main.py
+++ create_gapped_sentences/main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from create_gapped_sentences.gapped_sentences import generate_gapped_sentences
 from create_gapped_sentences.evaluate_gapped_sentences import evaluate_gapped_sentences
 from eval_suite.run_eval_suite import run_eval_suite
-from eval_suite.config import create_eval_config
+from config import create_eval_config
 
 config = create_eval_config(
     base_dir=Path(__file__).resolve().parent,
--- keyword_transformations/main.py
+++ keyword_transformations/main.py
@@ -7,7 +7,7 @@ from keyword_transformations.generate import generate_keyword_transformation
 from keyword_transformations.evaluate import evaluate_keyword_transformation
 
 from eval_suite.run_eval_suite import run_eval_suite
-from eval_suite.config import create_eval_config
+from config import create_eval_config
 
 
 config = create_eval_config(
