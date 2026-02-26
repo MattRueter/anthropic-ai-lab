bc0544ebf972cef2476db058f1f7e71148f5154d
Matt <84277551+MattRueter@users.noreply.github.com>
2026-02-26 15:18:08 +0100

    update main script to handle autocomplete or explicit dir_name as arg

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from pathlib import Path
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: eval <feature_name>")
+        print("Usage: eval <feature_name> OR  eval features/<feature_name>/")
         sys.exit(1)
 
-    feature = sys.argv[1]
-    module_path = f"features.{feature}.main"
+    raw_input = sys.argv[1]
+
+    #Normalize input
+    feature_name = Path(raw_input).name #unpacks name of feature folder
+
+    module_path = f"features.{feature_name}.main"
 
     project_root = Path(__file__).resolve().parent
   
